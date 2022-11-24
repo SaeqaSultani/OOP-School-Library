1dd90faa19a77e9fec5249af840543c680806510
Saeqa Sultani <saeqasultani@gmail.com>
2022-11-24 16:23:44 +0430

    Added linters file

--- student.rb
+++ student.rb
@@ -1,13 +1,12 @@
 require_relative 'person'
 
-class Student < Person 
+class Student < Person
+  def initialize(age, class_room, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @class_room = class_room
+  end
 
-    def initialize(classroom)
-        super(name, age, parent_permission)
-        @classroom = classroom
-      end
-
-    def play_hooky
+  def play_hooky
     '¯\(ツ)/¯'
-    end
+  end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,12 @@
 require_relative 'person'
 
 class Teacher < Person
-    def initialize(specialization)
-      super(name, age, parent_permission)
-      @specialization = specialization
-    end
-  
-    def can_use_services?
-      true
-    end
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
+    @specialization = specialization
   end
+
+  def can_use_services?
+    true
+  end
+end
