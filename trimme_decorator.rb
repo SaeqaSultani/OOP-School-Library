19b29ac9ccecd309788bc137f7ca095eacc77168
Saeqa Sultani <saeqasultani@gmail.com>
2022-11-26 18:23:25 +0430

    Edite the TrimmerDecorator file

--- trimme_decorator.rb
+++ trimme_decorator.rb
@@ -9,6 +9,15 @@ class TrimmerDecorator < BaseDecorator
   end
 
   def correct_name
-    super[0...10]
+    @nameable.correct_name.length > 10 ? @nameable.correct_name[0..9] : @nameable.correct_name
   end
 end
+
+person = Person.new(22, 'maximilianus')
+puts person.correct_name
+
+capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+
+capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
